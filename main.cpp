75a0d8814444bb3554bf36b168cbfc55f8fb5f88
yuchilin <dog0805@gmail.com>
2020-04-01 17:20:00 +0800

    pwm

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include "uLCD_4DGL.h"
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
-PwmOut PWM1(D6);
+PwmOut PWM1(D7);
+
 
 int main()
 {
@@ -13,5 +14,20 @@ int main()
     uLCD.text_height(4);
 
     uLCD.filled_rectangle(50, 50, 70,70, 0x00FF00);
+
+    while(1){
+        PWM1.period(0.001);
+        for(float i=0;i<=1;i+=0.1){
+            PWM1 = i;
+            wait(0.1);
+        }
+        for(float i=1;i>=0;i-=0.1){
+            PWM1=i;
+            wait(0.1);
+        }
+    }
+    
+    
+
     
 }
